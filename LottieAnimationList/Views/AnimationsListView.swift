242e3d0fdc022789f58173b686671233d64adc01
Dale Liu <YLiu@lcs.on.ca>
2024-01-29 08:48:37 -0500

    Organized project and created a list view to show interesting animations.

---
+++ LottieAnimationList/Views/AnimationsListView.swift
@@ -0,0 +1,18 @@
+//
+//  AnimationsListView.swift
+//  LottieAnimationList
+//
+//  Created by Yiren LIU on 2024-01-29.
+//
+
+import SwiftUI
+
+struct AnimationsListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AnimationsListView()
+}
